ec25e00d376045db5b4216536c30ea30066f907d
jamel kerr <jamel.kerr@yahoo.com>
2024-05-10 14:12:41 -0400

    add

--- GreetByName/index.js
+++ GreetByName/index.js
@@ -1,21 +1,7 @@
-"use strict"
-
-window.onload = init;
-
-function init() {
-
-    const onGreetUserBtn = document.getElementById("greetBtn");
-    
-    onGreetUserBtn.onclick = onClickMessage;
-
-}
-
-function onClickMessage () {
-
-    var userNameInput = document.getElementById("nameField");
-
-    var greetName = userNameInput.value;
-
-    alert ("Hello " + greetName)
-
-}
+const nameField = document.getElementById("nameField");
+const ageField = document.getElementById("ageField");
+let name = nameField.value;
+let age = ageField.value; // age is a string here
+let message =`Hi ${name}! I hear you are ${age} years old!`;
+const messagePara = document.getElementById("messagePara");
+messagePara.innerHTML = message
