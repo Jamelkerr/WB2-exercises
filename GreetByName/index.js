75e236b5517027db71377a7669d0131aef4c537a
jamel kerr <jamel.kerr@yahoo.com>
2024-05-10 11:44:58 -0400

    add

---
+++ GreetByName/index.js
@@ -0,0 +1,15 @@
+window.onload = init;
+
+function init(){
+
+    const greetBtnElement = document.getElementById("greetBtn");
+  
+
+    greetBtnElement.onclick = onGreetUserBtnClicked;
+}
+
+function onGreetUserBtnClicked(){
+    //console.log(nameField.value);
+    const nameField = document.getElementById("nameField");
+    alert(`Hello ${nameField.value}`);
+}
